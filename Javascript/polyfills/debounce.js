03feff3ccafcef0670652b848407ac42c6f3b828
swapnaBommu <swapnabommu8143@gmail.com>
2025-01-21 23:26:05 +0530

    debounce polyfill

---
+++ Javascript/polyfills/debounce.js
@@ -0,0 +1,9 @@
+const myDebounce = (cb,d) =>{
+    let timer;
+    return function(...args){
+        if(timer){
+            clearTimeout(timer);
+        }
+        timer = setTimeout(()=>{cb(...args)},d)
+    }
+}
