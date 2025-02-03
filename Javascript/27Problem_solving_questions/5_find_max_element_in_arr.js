6d27b9f40a46360caaca5a3a19fdba49290f6ebb
swapnaBommu <swapnabommu8143@gmail.com>
2025-02-03 18:40:12 +0530

    changed names of the files and is prime fucntion

--- Javascript/27Problem_solving_questions/5_find_max_element_in_arr.js
+++ Javascript/27Problem_solving_questions/5_find_max_element_in_arr.js
@@ -6,3 +6,13 @@ let a = [1,2,2,7,4,8,3,1,57,3];
 
 const maxElement = findMax(a);
 console.log(maxElement);
+
+//find max element in the arr
+let arr = [1,2,12,3,6,9];
+let max = 0;
+for(let i=0;i<arr.length;i++){
+    if(max < arr[i]){
+        max = arr[i];
+    }
+}
+console.log(max)
