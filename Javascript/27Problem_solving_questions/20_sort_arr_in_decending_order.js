f1defc63c05b058830b0bdd1ef2d171b9c69e07b
swapnaBommu <swapnabommu8143@gmail.com>
2025-02-04 12:28:41 +0530

    sort array in decending order of array

---
+++ Javascript/27Problem_solving_questions/20_sort_arr_in_decending_order.js
@@ -0,0 +1,19 @@
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] >= pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+const sortedArr = quickSort(arr);
+console.log(sortedArr);
